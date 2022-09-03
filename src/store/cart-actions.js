b0e23dee60c66a135af7bfdb49a025908f923e0b
vigneshactech2015 <85957001+vigneshactech2015@users.noreply.github.com>
2022-09-03 17:27:05 +0530

    Update cart-actions.js

--- src/store/cart-actions.js
+++ src/store/cart-actions.js
@@ -5,7 +5,7 @@ export const fetchCartData = () => {
   return async (dispatch) => {
     const fetchData = async () => {
       const response = await fetch(
-        'https://react-http-6b4a6.firebaseio.com/cart.json'
+        'https://reduxthunk-31d4e-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json'
       );
 
       if (!response.ok) {
@@ -49,7 +49,7 @@ export const sendCartData = (cart) => {
 
     const sendRequest = async () => {
       const response = await fetch(
-        'https://react-http-6b4a6.firebaseio.com/cart.json',
+        'https://reduxthunk-31d4e-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json',
         {
           method: 'PUT',
           body: JSON.stringify({
